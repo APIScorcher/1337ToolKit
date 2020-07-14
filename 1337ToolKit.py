c7b6e4758b722984b3b5124a7b29817b3f0236a1
APIScorcher <63946837+APIScorcher@users.noreply.github.com>
2020-07-14 12:04:50 +0500

    Add files via upload

---
+++ 1337ToolKit.py
@@ -0,0 +1,57 @@
+import time
+
+print("1337 Toolkit...")
+print("-----" * 6)
+time.sleep(3)
+print("Welcome to 1337 ToolKit made by  Predator.")
+time.sleep(5)
+option = ""
+print("")
+print("Choose an option:")
+option = input("""1.FBI Backdoor
+2.Instagram Database Fetcher
+3.SnapChat User Executer
+Option: """)
+###Launch backdoor z == fbi.tunnel(3) if fbi.tunnel == True: exec backdoor
+if option == "1":
+	print("----" * 7)
+	time.sleep(3)
+	print("Executing backdoor...")
+	time.sleep(10)
+	print("Logged into FBI Database as root.")
+	time.sleep(2)
+	print("Fetching FBI Servers...")
+	time.sleep(7)
+	print("Fetched FBI Information...")
+	print("""Information:
+Login:SkidKiller@fbi.gov
+Password:StupidFuckingSkids""")
+
+###Launch Insta z = bin/dev/insta.sql if True == launch database
+elif option == "2":
+  print("----" * 6)
+  time.sleep(7)
+  print("Launching Instagrams User Database...")
+  time.sleep(10)
+  print("Executing User Information...")
+  time.sleep(10)
+  print("Sending information to your computer...")
+  time.sleep(14)
+  print("Sent information to /usr/bin/TOO_MUCH_PORN")
+  
+###Launch Snap c == True: send snapchat.com/usrdta/bin.sql
+elif option == "3":
+  print("----" * 6)
+  time.sleep(3)
+  execute = input("Input a user you want to execute: ")
+  time.sleep(3)
+  print("Executing " + execute)
+  time.sleep(10)
+  print("Found " + execute + ("s address"))
+  time.sleep(7)
+  print("Sending FBI to " + execute + ("s  address"))
+  time.sleep(10)
+  print("Banning user from SnapChat...")
+  time.sleep(7)
+  print("Banned!")
+  
